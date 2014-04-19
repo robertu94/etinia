9ebd585bcf46f0aed093f1cc2812b4006941cc93
robertu94 <robertu@clemson.edu>
2014-04-19 11:23:43 -0400

    Stubbed map class

---
+++ map.py
@@ -0,0 +1,12 @@
+from character import Character
+
+class Map:
+   TheMap = [[0]] #tiles
+   Chars = [character]
+   def __init__(self):
+      TheMap = [[0,0,0,0,0][0,0,0,0,0][0,0,0,0,0][0,0,0,0,0][0,0,0,0,0][0,0,0,0,0]
+   def print_map(self)
+      """prints the map"""
+	  buf = TheMap
+	  #STUBBED 
+	  
