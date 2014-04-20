1f63e60bfb3e91c95124aacbee2c32f7ccc34971
User <User@bju.edu>
2014-04-19 23:48:58 -0400

    Moved movement from main to map controller.

--- main.py
+++ main.py
@@ -3,32 +3,18 @@
 from tkinter import *
 from map import Map
 from character import Character
+from mapcontroller import MapController
 
 world = Map(7, 5)
 hero = Character(2, 5)
 world.add_unit(hero)
 world.add_unit(Character(3, 5))
+map_controller = MapController(world)
 
 
 def key_pressed(event):
-    if event.keysym == "Left":
-        if world.valid_move(world.units[0].x -1, world.units[0].y, world.units[0]):
-            world.units[0].move(world.units[0].x-1, world.units[0].y)
-    if event.keysym == "Right":
-        if world.valid_move(world.units[0].x +1, world.units[0].y, world.units[0]):
-            world.units[0].move(world.units[0].x+1, world.units[0].y)
-    if event.keysym == "Up":
-        if world.valid_move(world.units[0].x, world.units[0].y-1, world.units[0]):
-            world.units[0].move(world.units[0].x, world.units[0].y -1 )
-    if event.keysym == "Down":
-        if world.valid_move(world.units[0].x, world.units[0].y+1, world.units[0]):
-            world.units[0].move(world.units[0].x, world.units[0].y+1)
+    map_controller.move(event.keysym)
     v.set(str(world))
-
-
-def key_released(event):
-    if event.keysym == "Left":
-        v.set("Nada")
 
 root = Tk()
 root.resizable(width=FALSE, height=FALSE)
@@ -38,5 +24,5 @@ v.set(str(world))
 w = Label(root, textvariable=v, height=24, width=80, bg='black', fg='white', font=("DejaVu Sans Mono", 12))
 w.pack()
 
-root.bind_all('<Key>', key_pressed)
+root.bind_all('<KeyPress>', key_pressed)
 root.mainloop()
