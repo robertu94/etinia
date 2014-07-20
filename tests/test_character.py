7dd698e066d3f023bcbf6d4b3c87f9ba9877c5e3
robertu94 <robertu@clemson.edu>
2014-07-19 20:22:22 -0400

    Started adding some unittests and cleaned up unnessiary actions

---
+++ tests/test_character.py
@@ -0,0 +1,12 @@
+'''
+File: test_character.py
+Author: Robert Underwood
+Description: a collection of test cases to verify character class functionality
+'''
+import unittest, unittest.mock, sys
+
+def test_character(unittest.TestCase):
+    """
+    A class to verify the functionality of the character
+    """
+
